a4b660495798bfa0a93abcb337225156f8faf169
pwnwriter <hey@pwnwriter.xyz>
2023-05-28 07:33:35 +0545

    feat: overall skel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main(){
-    println!("testttt");
+// Copyright PwnWriter // METIS Linux 2022 - present under the MIT License :) 
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = "None")]
+
+struct Arguments {
+    #[arg(short, long)]
+    /// App to swallow
+    app: String,
+
+}
+
+fn main() {
+    let arguments = Arguments::parse();
+    println!("{}", arguments.app);
 }
